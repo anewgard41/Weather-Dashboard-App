43040bc2105e7765223c550694c985580ccb53ea
Anders Newgard <anne9707@colorado.edu>
2023-08-31 16:45:09 -0400

    changed jQuery logic to web API methods to standardize what was being used across the JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var searchHistory = []
 
 // Grabbing the DOM elements
 
-var searchForm = $("#search-form");
+var searchForm = document.querySelector("#search-form");
 var searchInput = document.querySelector("#search-input");
 var searchHistoryContainer = document.querySelector("#history");
 var todayContainer = document.querySelector("#today");
@@ -173,15 +173,15 @@ function renderItems(city, data) {
 }
 
 
-function getCords(event) {
+function getCords(search) {
 
     // console logging the event in order to determine where the value of the search field is stored. 
 
-    event.preventDefault();
-    console.log(event)
-    console.log(event.target[0].value);
+    // event.preventDefault();
+    // console.log(event)
+    // console.log(event.target[0].value);
 
-    var search = event.target[0].value
+    // var search = event.target[0].value
     var apiUrl = `${weatherAPIRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`
 
     fetch(apiUrl)
@@ -245,13 +245,21 @@ function handleSearchHistoryClick(e) {
 }
 
 initSearchHistory();
-searchForm.on("submit", handleSearchFormSubmit);
+searchForm.addEventListener("submit", handleSearchFormSubmit);
+searchHistoryContainer.addEventListener("click", handleSearchHistoryClick);
 
-searchHistoryContainer.on("click", handleSearchHistoryClick);
 
-
-// searchForm.on("submit", getCords);
-
+// searchForm.addEventListener("submit", getCords);
 
 
 
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
